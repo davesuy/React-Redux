5581583bc421774601e2142f8c88f55599f6d5c8
Davesuy <davesuy@gmail.com>
2018-05-21 15:18:48 +0800

    Added files

---
+++ dev/js/actions/index.js
@@ -0,0 +1,8 @@
+export const selectUser = (user) => {
+	console.log("You clicked on User: ", user.first);
+
+	return {
+		type: "USER_SELECTED",
+		payload: user
+	}
+};
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function() {
+	return [
+		{
+			id: 1,
+			first: "Dave",
+			last: "Ramirez",
+			age: 29,
+			description: "Dave is a React developer and Producer",
+			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+		},
+		{
+			id: 2,
+			first: "Mark",
+			last: "Sanchez",
+			age: 27,
+			description: "Mark is a React developer and Producer",
+			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+		},
+		{
+			id: 3,
+			first: "Micheal",
+			last: "Cruz",
+			age: 26,
+			description: "Micheal is a React developer and Producer",
+			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+		}
+	]
+}
